8ff56b3a4e609b0953171d06892a03884e620eba
nbenns <nigel.benns@gmail.com>
2021-06-09 00:07:47 -0400

    Fix tests

--- src/test/scala/day1/package.scala
+++ src/test/scala/day1/package.scala
@@ -2,7 +2,7 @@ import zio.test.suite
 
 package object day1 {
   val testSuite = suite("Day1")(
-//    Part1Spec.testSuite,
-//    Part2Spec.testSuite
+    Part1Spec.testSuite,
+    Part2Spec.testSuite
   )
 }
